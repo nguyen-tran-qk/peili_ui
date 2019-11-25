12179cbc84cee01962dd35fc2aef00b20e38fc6e
Duc Toan Thanh <toanth@metropolia.fi>
2019-11-25 16:35:32 +0200

    Initial fix UI homepage

--- src/components/home-card-items/home-card-items.component.tsx
+++ src/components/home-card-items/home-card-items.component.tsx
@@ -2,18 +2,22 @@ import React from 'react';
 import './home-card-items.styles.scss';
 
 import { FaHeart } from 'react-icons/fa';
+import { url } from 'inspector';
 
 const HomeCardItems = (props: any) => {
   const { image } = props;
   return (
     <div className="home-card-items">
-      <div className="home-card-item-info">
-        <h3 className="home-card-items-title">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, ullam?</h3>
-        <p className="home-card-items-date">sep 16, 2019</p>
-      </div>
-      <div className="home-card-item-img-container">
-        <img src={image} alt="card-items" />
+      <div className="home-card-item-img-container" style={{backgroundImage: `url(${image})`}}>
         <FaHeart className="heart-icon" />
+      </div>
+      <div className="home-card-heading">
+        <img src={image} alt="card-items" />
+        <h4>Peili</h4>
+      </div>
+      <div className="home-card-item-info">
+        <h3 className="home-card-items-title">Lorem ipsum dolor sit amet consectetur adipisicing elit?</h3>
+        <p className="home-card-items-date">sep 16, 2019</p>
       </div>
     </div>
   );
--- src/components/tabbar/tabbar.components.tsx
+++ src/components/tabbar/tabbar.components.tsx
@@ -18,19 +18,19 @@ const TabBar = (props: any) => {
       <div className={`tabbar-item ${path === '/tests' ? 'tabbar-active' : null}`}>
         <Link to="/tests">
           <FaSignal className="tabbar-icon" />
-          <h3 className="tabbar-title">tests</h3>
+          <h3 className="tabbar-title">Tests</h3>
         </Link>
       </div>
       <div className={`tabbar-item ${path === '/organizations' || path.includes('/organizations') ? 'tabbar-active' : null}`}>
         <Link to="/organizations">
           <FaRegCommentAlt className="tabbar-icon" />
-          <h3 className="tabbar-title">Organizations</h3>
+          <h3 className="tabbar-title">Organisation</h3>
         </Link>
       </div>
       <div className={`tabbar-item ${path === '/profile' || path.includes('/profile/') ? 'tabbar-active' : null}`}>
         <Link to="/profile">
           <FaUser className="tabbar-icon" />
-          <h3 className="tabbar-title">profile</h3>
+          <h3 className="tabbar-title">Profile</h3>
         </Link>
       </div>
     </div>
