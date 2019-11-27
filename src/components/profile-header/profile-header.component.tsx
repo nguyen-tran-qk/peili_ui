240319bb0dd6a13b98988d32e65c5472c6e2a92e
Duc Toan Thanh <toanth@metropolia.fi>
2019-11-27 16:53:38 +0200

    Handle logic redeem rewards

--- src/components/profile-header/profile-header.component.tsx
+++ src/components/profile-header/profile-header.component.tsx
@@ -5,27 +5,33 @@ import settingImg from '../../assets/images/settings.png';
 
 import { Link } from 'react-router-dom';
 
+import { PeiliUserConsumer } from '../../context/userContext';
+
 const ProfileHeader = () => {
   return (
-    <div className="profile-header">
-      <Link to="/profile/settings">
-        <img src={settingImg} alt="settings" className="settings-icon" />
-      </Link>
+    <PeiliUserConsumer>
+      {(value: any) => {
+        const { user } = value;
+        return (
+          <div className="profile-header">
+            <Link to="/profile/settings">
+              <img src={settingImg} alt="settings" className="settings-icon" />
+            </Link>
 
-      <div className="profile-img-container">
-        <img src="https://randomuser.me/api/portraits/med/men/75.jpg" alt="avatar" />
-      </div>
-      <div className="profile-info">
-        <h2 className="name">michael jackson</h2>
-        <p className="email">michael@gmail.com</p>
-        <span className="exp">
-          <span>1000 EXP</span>
-        </span>
-        {/* <Link to="/profile">
-          get rewards <FaArrowRight />
-        </Link> */}
-      </div>
-    </div>
+            <div className="profile-img-container">
+              <img src="https://randomuser.me/api/portraits/med/men/75.jpg" alt="avatar" />
+            </div>
+            <div className="profile-info">
+              <h2 className="name">{user.firstName} {user.lastName}</h2>
+              <p className="email">{user.email}</p>
+              <span className="exp">
+                <span>{user.exp} EXP</span>
+              </span>
+            </div>
+          </div>
+        );
+    }}
+    </PeiliUserConsumer>
   );
 };
 
--- src/components/profile-rewards/profile-rewards.component.tsx
+++ src/components/profile-rewards/profile-rewards.component.tsx
@@ -1,32 +1,49 @@
 import React, { useState }  from 'react';
 import './profile-rewards.styles.scss';
 
-import productImg from '../../assets/images/img-product.png';
+import { PeiliUserConsumer } from '../../context/userContext';
 
 const Rewards = (props: any) => {
-  const { reward } = props;
   const [ isRedeemed, redeem ] = useState(false);
-
-  const redeemReward = () => {
-    redeem(true);
-  };
-  
+  const [ errorRedeem, setError ] = useState(false);
   return (
-    <div className="rewards">
-      <div className="rewards-img-container">
-        <img src={reward.image} alt="product" />
-      </div>
-      <div className="rewards-info">
-        <p className="rewards-subtitle">{reward.description}</p>
-        <p className="rewards-exp">{reward.exp} EXP</p>
-        <button 
-          onClick={redeemReward} 
-          className="rewards-button" 
-          style={{backgroundColor: isRedeemed ? '#000' : '#fff', color: isRedeemed ? '#fff' : '#000'}}>
-            redeem{isRedeemed ? 'ed' : ''}
-        </button>
-      </div>
-    </div>
+    <PeiliUserConsumer>
+      {(value: any) => {
+        const { user, substractEXP } = value;
+        const { reward } = props;
+        const redeemReward = (exp: any) => {
+          if (user.exp > exp) {
+            redeem(true); // change button style
+            substractEXP(exp); // update user data
+          } else {
+            setError(true); // render error in reward card
+          }
+        };
+        return (
+          <div className="rewards">
+            <div className="rewards-img-container">
+              <img src={reward.image} alt="product" />
+            </div>
+            <div className="rewards-info">
+              <p className="rewards-subtitle">{reward.description}</p>
+              <p className="rewards-exp">{reward.exp} EXP</p>
+              <p 
+                style={{display: errorRedeem ? 'block' : 'none', color: 'red', marginBottom: '10px'}}
+              >
+                You don't have enough EXP to redeem
+              </p>
+              <button 
+                onClick={() => redeemReward(reward.exp)}
+                className="rewards-button" 
+                style={{backgroundColor: isRedeemed ? '#000' : '#fff', color: isRedeemed ? '#fff' : '#000'}}
+              >
+                redeem{isRedeemed ? 'ed' : ''}
+              </button>
+            </div>
+          </div>
+        );
+    }}
+    </PeiliUserConsumer>
   );
 };
 
