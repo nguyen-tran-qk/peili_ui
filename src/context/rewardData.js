9b76707903c2af321782876a16180b4ca977b956
Duc Toan Thanh <toanth@metropolia.fi>
2019-11-27 15:34:47 +0200

    Reward rendering logic + styling

---
+++ src/context/rewardData.js
@@ -0,0 +1,27 @@
+import productImg from '../assets/images/img-product.png';
+
+const rewardData = [
+    {   
+        id: 1,
+        exp: 100,
+        createdAt: 'Sep 16, 2019',
+        image: productImg,
+        description: "ELIXIA exclusive 3-month membership",
+    },
+    {   
+        id: 2,
+        exp: 200,
+        createdAt: 'Sep 16, 2019',
+        image: productImg,
+        description: "ELIXIA exclusive 3-month membership",
+    },
+    {   
+        id: 3,
+        exp: 300,
+        createdAt: 'Sep 16, 2019',
+        image: productImg,
+        description: "ELIXIA exclusive 3-month membership",
+    },
+];
+
+export default rewardData;
