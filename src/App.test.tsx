284457433a5bef5bab125c4e151f8085508d7ede
Nguyen Tran <kevtran07@gmail.com>
2019-12-10 00:58:05 +0200

    fixed CI build fails

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,7 +4,4 @@ import {MemoryRouter} from 'react-router-dom';
 import App from './App';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<MemoryRouter><App /></MemoryRouter>, div);
-  ReactDOM.unmountComponentAtNode(div);
 });
